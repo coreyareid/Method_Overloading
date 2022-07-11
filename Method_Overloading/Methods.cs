5d7a3c2a1ab86ad94273a1f9dc68096165d39b85
coreyareid <coreyareid@bellsouth.net>
2022-07-11 11:33:42 -0500

    Method overloading exercise

---
+++ Method_Overloading/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Method_Overloading
+{
+    public class Methods
+    {
+        // Exercise Method Overloading
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool choice)
+        {
+            int answer = x + y;
+
+
+            if (choice == true && answer > 1)
+            {
+                return $"You have {answer} dollars.";
+            }
+            else if (choice == true && answer == 1)
+            {
+                return $"You have {answer} dollar.";
+            }
+            else if (choice == true && answer < 1)
+            {
+                return $"You have {answer} dollars.";
+            }
+            else
+            {
+                return answer.ToString();
+            }
+        }
+    }
+}
+
